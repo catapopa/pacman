e6a2aac6ae2621bfb436853a79e862761c3e1717
Popa Catalina <cataqoq@gmail.com>
2018-11-20 23:23:34 +0200

    initializable

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,28 +6,35 @@ import javafx.scene.layout.GridPane;
 
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 
-public class Controller {
+public class Controller implements Initializable{
 
     Repository repository;
 
     @FXML
     GridPane gridPane;
 
-
-    public Controller(Repository repository) throws FileNotFoundException {
-        this.repository = repository;
-        this.fileToMemory();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.repository = new Repository();
+        try {
+            this.fileToMemory();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public Cell[][] fileToMemory() throws FileNotFoundException {
         return repository.fileToMemory(this.gridPane);
     }
 
-   /* public Repository drawTable() {
-        //Cell[][] file = // iterez matricea, create block, add to table, root.add(block, row, col)i, j
-    }*/
+    public void drawTable() throws FileNotFoundException {
+       Cell[][] file = this.fileToMemory();
+        //Arrays.stream(file).map()
+       // iterez matricea, create block, add to table, root.add(block, row, col)i, j
+    }
 
 }

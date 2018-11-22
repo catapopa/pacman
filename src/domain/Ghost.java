a64609b7207f243612a1647c114e709494ea3eba
Popa Catalina <cataqoq@gmail.com>
2018-11-22 15:42:33 +0200

    Ghost

---
+++ src/domain/Ghost.java
@@ -0,0 +1,20 @@
+package domain;
+
+import javafx.scene.paint.Color;
+
+import java.util.Random;
+
+public class Ghost extends Cell {
+
+    Random random = new Random();
+
+    private Ghost() {
+        super(0, 0, Color.ORANGERED);
+    }
+
+    public void init(int x, int y) {
+        super.setX(x);
+        super.setY(y);
+        super.getNode().relocate(x, y);
+    }
+}
